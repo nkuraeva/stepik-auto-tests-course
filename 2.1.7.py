2d214d042d2a828addef3e9dfec9d559a7e00a0e
nkuraeva <kuraeva@gmail.com>
2021-01-21 13:10:23 +0100

    add finding treasure with get_attribute

---
+++ 2.1.7.py
@@ -0,0 +1,28 @@
+import math
+from selenium import webdriver
+import time
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    link = "http://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    x_element = browser.find_element_by_id("treasure")
+    x = x_element.get_attribute("valuex")
+    y = calc(x)
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+    option1 = browser.find_element_by_id("robotCheckbox")
+    option1.click()
+    option2 = browser.find_element_by_id("robotsRule")
+    option2.click()
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
